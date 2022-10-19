02648207d206c389c476935c3233d0bef70a739e
Metehan Topaç <metehan.topac02@gmail.com>
2022-10-19 14:29:40 +0200

    First publish

---
+++ Flowerland_Horror/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+
+    [Header("Health Settings")]
+    public GameObject healthSlider;
+    public float health = 100;
+    public float healthMax = 100;
+    public float healValue = 5;
+    public float secondToHeal = 10;
+
+    [Header("Flashlight Battery Settings")]
+    public GameObject Flashlight;
+    public GameObject batterySlider;
+    public float battery = 100;
+    public float batteryMax = 100;
+    public float removeBatteryValue = 0.05f;
+    public float secondToRemoveBaterry = 5f;
+
+    [Header("Audio Settings")]
+    public AudioClip enemyNoise;
+
+    [Header("UI Settings")]
+    public GameObject inGameMenuUI;
+    public GameObject pickUpUI;
+    public GameObject finishedGameUI;
+    public GameObject pagesCount;
+    public bool paused;
+
+    
+    void Start()
+    {
+        // set initial health values
+        health = healthMax;
+        battery = batteryMax;
+
+        healthSlider.GetComponent<Slider>().maxValue = healthMax;
+        healthSlider.GetComponent<Slider>().value = healthMax;
+
+        // set initial battery values
+        batterySlider.GetComponent<Slider>().maxValue = batteryMax;
+        batterySlider.GetComponent<Slider>().value = batteryMax;
+
+        // start consume flashlight battery
+        StartCoroutine(RemoveBaterryCharge(removeBatteryValue, secondToRemoveBaterry));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator RemoveBaterryCharge(float value, float time)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(time);
+
+            Debug.Log("Removing baterry value: " + value);
+
+            if (battery > 0)
+                battery -= value;
+            else
+                Debug.Log("The flashlight battery is out");
+        }
+    }
+
+}
